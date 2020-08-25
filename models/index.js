ea5e18cca4f4236e71d1ddcb8bcc0a923a5bc87f
Ohdongchi <dhqud1234@naver.com>
2020-08-25 13:21:54 +0900

    third commit

--- models/index.js
+++ models/index.js
@@ -14,4 +14,7 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 db.User = require("./user")(sequelize, Sequelize);
 db.Video = require("./video_board")(sequelize, Sequelize);
+
+db.User.hasMany(db.Video);
+db.Video.belongsTo(db.User);
 module.exports = db;
