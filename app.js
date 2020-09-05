bab4be6dc844254635389d81fcd6d31e76dad5ae
Ohdongchi <dhqud1234@naver.com>
2020-09-05 13:17:01 +0900

    6th commit

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.set("port", process.env.PORT);
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "front")));
+app.use("/video", express.static(path.join(__dirname, "uploads")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
